f1cc6f860e0b262c0702ba05cd7ec0a3ce6b1d86
Denys Kukhar <denys.kukhar@student.tuke.sk>
2018-09-16 15:48:35 +0200

    trying to resolve issue with permissions attating to use dbs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
     useNewUrlParser: true,
-    dbName: 'TodoApp'
+    // dbName: 'TodoApp'
 });
 
 module.exports = {
